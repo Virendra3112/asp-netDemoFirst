159e4c8c8903de18c796c687a2aa9d9283a3780b
Virendra <virendra.thakur31@gmail.com>
2021-10-13 15:38:44 +0530

    Product model updated.

--- CalatlogAPI/Entities/Product.cs
+++ CalatlogAPI/Entities/Product.cs
@@ -1,15 +1,11 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CalatlogAPI.Entities
 {
     public class Product
     {
         [BsonId]
-        [BsonRequired]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
