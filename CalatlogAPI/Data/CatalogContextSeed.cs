b49df6ea3ae785858e496baf3fb11554b1f01ba2
Virendra <virendra.thakur31@gmail.com>
2021-10-13 16:01:41 +0530

    CatalogContextSeed updated.

--- CalatlogAPI/Data/CatalogContextSeed.cs
+++ CalatlogAPI/Data/CatalogContextSeed.cs
@@ -1,6 +1,7 @@
 ﻿using CalatlogAPI.Entities;
 using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
 
 namespace CalatlogAPI.Data
 {
@@ -8,10 +9,32 @@ namespace CalatlogAPI.Data
     {
         public static void SeedData(IMongoCollection<Product> productCollection)
         {
-            if (productCollection != null)
+            bool existProduct = productCollection.Find(s => true).Any();
+
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProduct());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProduct()
+        {
+            return new List<Product>
             {
 
-            }
+               new Product{
+                   Id="001",
+                   Name = "Test1",
+                   Summary="test Summary 1",
+                   Category="test Category 1",
+                   Description = "Description 1",
+                   ImageFile = "test imagefile",
+                   Path = ""
+
+                }
+
+
+            };
         }
     }
 }
