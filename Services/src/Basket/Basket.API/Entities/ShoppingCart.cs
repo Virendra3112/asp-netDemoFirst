723f25e29d3882d70c8b3cec34fa38959048894d
Virendra <virendra.thakur31@gmail.com>
2021-10-20 15:50:26 +0530

    ShoppingCart added.

---
+++ Services/src/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+        
+        public ShoppingCart(string UserName)
+        {
+
+        }
+        public string UserName { get; set; }
+        public List<ShoppingCart> List { get; set; }
+    }
+}
