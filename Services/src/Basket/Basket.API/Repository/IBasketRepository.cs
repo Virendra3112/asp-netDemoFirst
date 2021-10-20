967b11b805803afcca40d1e1a7d4874265574095
Virendra <virendra.thakur31@gmail.com>
2021-10-20 16:46:34 +0530

    IBasketRepository added.

---
+++ Services/src/Basket/Basket.API/Repository/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repository
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string userName);
+    }
+}
