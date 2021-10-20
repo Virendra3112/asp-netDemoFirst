c88b277ede13b0ae23a76fe35b6c90bf8eaa324b
Virendra <virendra.thakur31@gmail.com>
2021-10-20 16:46:46 +0530

    BasketRepository added.

---
+++ Services/src/Basket/Basket.API/Repository/BasketRepository.cs
@@ -0,0 +1,44 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repository
+{
+    public class BasketRepository : IBasketRepository
+    {
+        IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (!string.IsNullOrEmpty(basket))
+                return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+
+            else
+                return null;
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await _redisCache.SetStringAsync(shoppingCart.UserName
+                , JsonConvert.SerializeObject(shoppingCart));
+
+            return await GetBasket(shoppingCart.UserName);
+        }
+    }
+}
