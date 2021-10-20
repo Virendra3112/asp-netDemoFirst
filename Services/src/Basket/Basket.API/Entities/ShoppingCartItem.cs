3ca25b48d3cd06a820972f3f5868ccfc44c5128a
Virendra <virendra.thakur31@gmail.com>
2021-10-20 16:46:01 +0530

    ShoppingCartItem model added.

---
+++ Services/src/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public string ItemName { get; set; }
+        public int ItemId { get; set; }
+        public string ItemColor { get; set; }
+        public string ItemPrice { get; set; }
+    }
+}
